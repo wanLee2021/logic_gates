1f12e5c58af990bed9133a968e3325ee9da2e25a
wanLee2021 <onebyonly@outlook.com>
2022-12-28 23:42:14 +0800

    :construction: feat:ci/cd

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+#![doc(html_logo_url = "https://d30y9cdsu7xlg0.cloudfront.net/png/411962-200.png")]
+/// 实现一个逻辑门and，将这两个作为输入，并返回一个位作为输出
+pub fn and(a: u8, b: u8) -> u8 {
+
+    match (a, b) {
+        (1, 1) => 1,
+        _ => 0
+    }
+}
+
+/// 实现一个逻辑门xor，将两个位作为输入，并返回一个位作为输出
+pub fn xor(a: u8, b: u8) -> u8 {
+
+    match (a, b) {
+        (1, 0) | (0, 1) => 1,
+        _ => 0
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::{xor, and};
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_and() {
+        assert_eq!(1, and(1, 1));
+        assert_eq!(0, and(0, 1));
+        assert_eq!(0, and(1, 0));
+        assert_eq!(0, and(0, 0));
+    }
+
+    #[test]
+    fn test_xor() {
+        assert_eq!(1, xor(1, 0));
+        assert_eq!(0, xor(0, 0));
+        assert_eq!(0, xor(1, 1));
+        assert_eq!(1, xor(0, 1));
     }
 }
